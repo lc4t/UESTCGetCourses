60372aeadbbb6bced9623fca537f3bc852022be7
lc4t <lc4t0.0@gmail.com>
2017-04-08 02:55:40 +0800

    fix login bug

--- get_courses.py
+++ get_courses.py
@@ -102,7 +102,7 @@ class uestc():
         }
         login_r = self.r.post(url=self.loginURL, data=post_data, headers=self.headers)
         result = login_r.text
-        AC = re.findall(u'href="http://eams.uestc.edu.cn/eams/"><b>教务系统', result)
+        AC = re.findall(u'https://mail.std.uestc.edu.cn/"><br"', result)
         if AC:
             res = self.get_eas()
             return json.dumps({'JSESSIONID': res})
